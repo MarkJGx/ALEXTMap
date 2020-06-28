85c5a195607b34f4e4274bbdf3331c2b2534cdf1
MarkJGx <38701409+MarkJGx@users.noreply.github.com>
2020-06-28 19:35:32 +0300

    fixed benchmark

--- Source/ALEXTMap/Private/ALEXTMap.cpp
+++ Source/ALEXTMap/Private/ALEXTMap.cpp
@@ -13,15 +13,17 @@ void FALEXTMapModule::StartupModule()
 {
 #if ALEX_BENCHMARK
 	{
+		const int32 Max = 10000000;
+		
 		TMap<int32, int32> Map;
-		for (long idx = 0; idx < 10000000; idx++) {
-			Map.Add(idx, idx);
+		for (int32 Index = 0; Index < Max; Index++) {
+			Map.Add(Index, Index);
 		}
 
 		auto TMapStartTime = std::chrono::high_resolution_clock::now();
-		long x = 0;
-		for (long idx = 0; idx < 10000000; idx++) {
-			x =  Map[idx];
+		int32 x = 0;
+		for (int32 Index = 0; Index < Max; Index++) {
+			x =  Map[Index];
 		}
 		auto TMapEndTime = std::chrono::high_resolution_clock::now();
 		double TMapLookUpTime =
@@ -29,18 +31,18 @@ void FALEXTMapModule::StartupModule()
 		UE_LOG(LogTemp, Warning, TEXT("TMap Lookup Time: %f"), TMapLookUpTime);
 
 		TAlexMap<int32, int32> AlexMap;
-		for (long idx = 0; idx < 10000000; idx++) {
-			AlexMap.Add(idx, idx);
+		for (int32 Index = 0; Index < Max; Index++) {
+			AlexMap.Add(Index, Index);
 		}
 
 		auto TAlexStartTime = std::chrono::high_resolution_clock::now();
 		x = 0;
-		for (long idx = 0; idx < 10000000; idx++) {
-			x =  AlexMap[idx];
+		for (int32 Index = 0; Index < Max; Index++) {
+			x =  AlexMap[Index];
 		}
 		auto TAlexEndTime = std::chrono::high_resolution_clock::now();
 		double TAlexLookUpTime =
-            std::chrono::duration_cast<std::chrono::nanoseconds>(TAlexEndTime - TMapStartTime).count();
+            std::chrono::duration_cast<std::chrono::nanoseconds>(TAlexEndTime - TAlexStartTime).count();
 		UE_LOG(LogTemp, Warning, TEXT("TAlexMap Lookup Time: %f"), TAlexLookUpTime);
 		
 	}
